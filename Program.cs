0c8259c732884071d3e0197e4e60553213e71e6e
Matt Bishop <withinfocus@gmail.com>
2021-02-15 12:06:10 -0500

    Statiq setup and deployment (#2)

---
+++ Program.cs
@@ -0,0 +1,24 @@
+using Statiq.App;
+using Statiq.Common;
+using Statiq.Web;
+using System.Threading.Tasks;
+
+namespace withinfocus
+{
+    public class Program
+    {
+        public static async Task<int> Main(string[] args)
+        {
+            return await Bootstrapper
+                .Factory
+                .CreateWeb(args)
+                .DeployToGitHubPagesBranch(
+                    "withinfocus",
+                    "withinfocus.github.io",
+                    Config.FromSetting<string>("GITHUB_TOKEN"),
+                    "main"
+                )
+                .RunAsync();
+        }
+    }
+}
